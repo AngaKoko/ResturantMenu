281de1cc967301d250b54bd146c8331fd776a34e
Anga <angakoko@gmail.com>
2020-03-14 22:45:39 +0100

    add comment of file

--- crud.py
+++ crud.py
@@ -1,3 +1,7 @@
+"""
+Perform crude operations on database
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -7,35 +11,42 @@ Base.metadata.bind=engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-def createRestaurant(rName):
-    restaurant = Restaurant(name=rName)
+#function adds a new resturant to database
+def createRestaurant(_name):
+    restaurant = Restaurant(name=_name)
     session.add(restaurant)
     session.commit()
 
+#Fuction queries all resturants in the database
 def queryAllRestaurants():
     restarants = session.query(Restaurant).all()
     for restaurant in restarants:
         print(restaurant.name)
 
-def getItemInAllRestaurants(itemName):
-    items = session.query(MenuItem).filter_by(name=itemName)
+
+#get details of item in all restaurants
+def getItemInAllRestaurants(item_name):
+    items = session.query(MenuItem).filter_by(name=item_name)
     for item in items:
         print(item.id)
         print(item.price)
         print(item.restaurant.name)
         print("\n")
 
+#Adds a new item to a restaurant
 def addItemToRestaurant(_name, _description, _course, _price, _restaurant):
     item = MenuItem(name=_name, description=_description, price=_price, course=_course, restaurant=_restaurant)
     session.add(item)
     session.commit()
 
+#Updates itme price using item id
 def updateItemPrice(_id, _price):
     item = session.query(MenuItem).filter_by(id = _id).one()
     item.price = _price
     session.add(item)
     session.commit()
 
+#deltes item from database using item id
 def deleteItem(_id):
     item = session.query(MenuItem).filter_by(id = _id).one()
     session.delete(item)
